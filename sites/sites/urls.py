fa1a1c825827a3980e9e360202d81fc070a05db5
Deni <denibatyzhev@gmail.com>
2023-09-18 22:48:08 +0500

    demo statistics

--- sites/sites/urls.py
+++ sites/sites/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.static import serve
+from room.views import simple_chart_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('room/', include('room.urls')),
     path("ckeditor5/", include('django_ckeditor_5.urls')),
+    path('test/', simple_chart_view),
     path('', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
